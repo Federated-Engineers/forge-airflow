37ba70c183ab331f4e89564964c8838a5a1687d8
chik0di <chikodiobu@gmail.com>
2026-04-25 21:09:49 +0100

    deprecated:airflow.decorators.dag; in-use:airflow.sdk.dag

--- dags/bbss_workflow_orchestration.py
+++ dags/bbss_workflow_orchestration.py
@@ -1,4 +1,4 @@
-from airflow.decorators import dag, task
+from airflow.sdk import dag, task
 from airflow.models import Variable
 from pendulum import datetime as pendulum_datetime
 from datetime import timedelta
