32b93dc2b39b838a6722fea0af278159e1995514
Afeez Olasunkanmi <olasunkanmiabidemia@gmail.com>
2026-04-26 20:21:02 +0100

    Update km-finance

--- business_logic/kinetic_motors/extract_load.py
+++ business_logic/kinetic_motors/extract_load.py
@@ -4,6 +4,7 @@ import logging
 import boto3
 from airflow.sdk import Variable
 
+from plugins.extract_load_portugal import extract_portugal, write_df_to_sheet
 from plugins.google_sheets import authenticate_google_sheet
 
 log = logging.getLogger(__name__)
@@ -12,45 +13,19 @@ log = logging.getLogger(__name__)
 VARIABLE_KEY = "km_loaded_files"
 
 
-def extract_portugal(file_key, bucket):
-    log.info(f"Reading {file_key} from S3")
-
-    df = wr.s3.read_parquet(f"s3://{bucket}/{file_key}")
-    filtered_df = df[df["plant_country"].str.strip().str.upper() == "PORTUGAL"]
-
-    if filtered_df.empty:
-        log.warning("No Portugal records found.")
-
-    return filtered_df
-
-
-def write_df_to_sheet(df, spreadsheet_id, sheetname, scopes):
-    """Appends data to google sheets"""
-    if df.empty:
-        log.info("No data to write, skipping...")
-        return
-
-    google_cred = authenticate_google_sheet(scopes)
-
-    worksheet = google_cred.open_by_key(spreadsheet_id).worksheet(sheetname)
-    first_row = worksheet.row_values(1)
-
-    if not first_row:
-        log.info("Sheet is empty, writing headers and data...")
-        worksheet.append_rows([df.columns.tolist()] + df.values.tolist())
-    else:
-        log.info("Sheet has data, appending rows only...")
-        worksheet.append_rows(df.values.tolist())
-
-
 def extract_load_portugal(spreadsheet_id, sheetname):
-    """Extracts Portugal data from s3 and loads to google sheet"""
+    """Extracts Portugal data from s3 and loads to google sheet.
 
+    Args:
+        spreadsheet_id: The spreadsheet id for the Google sheet
+        sheetname: The sheetname where data will be written
+    """
     config = json.loads(Variable.get("km_config"))
     bucket = config["bucket"]
     folder = config["folder"]
     scopes = config["scopes"]
 
+    google_cred = authenticate_google_sheet(scopes)
     s3 = boto3.client("s3")
 
     all_files = s3.list_objects_v2(
@@ -59,6 +34,7 @@ def extract_load_portugal(spreadsheet_id, sheetname):
     ).get("Contents", [])
 
     loaded_files = json.loads(Variable.get(VARIABLE_KEY, default="[]"))
+    log.info(f"Writing to sheet: {spreadsheet_id} | {sheetname}")
 
     for file in all_files:
         file_key = file["Key"]
@@ -68,10 +44,10 @@ def extract_load_portugal(spreadsheet_id, sheetname):
             continue
 
         df = extract_portugal(file_key, bucket)
-        write_df_to_sheet(df, spreadsheet_id, sheetname, scopes)
+        log.info(f"Writing to sheet: {spreadsheet_id} | {sheetname}")
+        write_df_to_sheet(df, spreadsheet_id, sheetname, google_cred)
 
         loaded_files.append(file_key)
-        log.info(f"Loading {file_key}, {len(df)} rows")
-
         Variable.set(VARIABLE_KEY, json.dumps(loaded_files))
-        log.info(f'Successfully loaded {file_key}, {len(df)} rows updated')
+
+        log.info(f"Successfully loaded {file_key}, {len(df)} rows updated")
