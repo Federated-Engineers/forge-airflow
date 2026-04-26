0f8f38fc0800fe5ad4eb128d3dc212de3553a908
Olalekan Rasaq <orasaq377@stu.ui.edu.ng>
2026-04-26 13:44:28 +0100

    Introduce default args and change task name

--- dags/glaciair_logistic.py
+++ dags/glaciair_logistic.py
@@ -5,16 +5,23 @@ from pendulum import datetime
 from business_logic.glaciair_logistic.consolidate_glaciair_data import \
     load_gsheet_to_s3
 
+default_args = {
+    "owner": "Federated-Engineers",
+    "retries": 2,
+    "retry_delay": datetime.timedelta(seconds=10)
+}
+
 with DAG(
     dag_id='glaciair-logistics-data-sync',
+    default_args=default_args,
     start_date=datetime(2026, 4, 1),
     catchup=False,
     schedule="0 5 * * *",  # Everyday at 05:00
     tags=["forge", "data-sync", "glaciair", "google-sheets"]
 ):
-    run_pipeline = PythonOperator(
+    load_glaciair_sheets_s3 = PythonOperator(
         task_id="load_glaciair_sheets_s3",
         python_callable=load_gsheet_to_s3
     )
 
-    run_pipeline
+    load_glaciair_sheets_s3
