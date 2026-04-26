b0ae81c316026619be9276924ea6206b3bebade8
Chika Onyenenue <chikaonyenenue@gmail.com>
2026-04-26 20:04:08 +0200

    fixing PR comments

--- dags/linen_insight_dags.py
+++ dags/linen_insight_dags.py
@@ -2,9 +2,30 @@ from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from pendulum import datetime
 
-from business_logic.linen_luxury.extraction import extraction
+from business_logic.linen_luxury.extraction import postgress_extraction
+from business_logic.linen_luxury.extraction import google_extraction
 from business_logic.linen_luxury.load import move_file_s3
-from business_logic.linen_luxury.transform import transformation
+
+
+def run_all_scripts():
+    """
+    Executes the extraction and loading scripts for Liffey Linens.
+    """
+    postgres_data = postgress_extraction()
+    google_data = google_extraction()
+
+    move_file_s3(
+        google_data,
+        "lll/influencers_data/influencer_data.parquet",
+        "influencer_transactions"
+    )
+
+    move_file_s3(
+        postgres_data,
+        "lll/orders_data/orders_data.parquet",
+        "order_transactions"
+    )
+
 
 with DAG(
     dag_id="Liffey_Linens_Insights",
@@ -12,24 +33,6 @@ with DAG(
     schedule="0 */1 * * *",
     catchup=False
 ) as dag:
-
-    def run_all_scripts():
-        google_data, postgres_data = extraction()
-
-        influencer_data, orders_data = transformation(
-            google_data, postgres_data)
-
-        move_file_s3(
-            influencer_data,
-            "lll/influencers_data/influencer_data.parquet",
-            "influencer_transactions"
-        )
-
-        move_file_s3(
-            orders_data,
-            "lll/orders_data/orders_data.parquet",
-            "order_transactions"
-        )
 
     run_etl_script_to_s3 = PythonOperator(
         task_id="run_all_scripts",
