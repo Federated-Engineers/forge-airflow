52fac51e1818ae8ef05d243bce0a9efc4275cbe7
chik0di <chikodiobu@gmail.com>
2026-04-27 17:12:46 +0100

    Update DAG approach remove task decorator approach now instantiates DAG object overall DAG simplification

--- dags/bbss_workflow_orchestration.py
+++ dags/bbss_workflow_orchestration.py
@@ -1,53 +1,32 @@
-from airflow.sdk import dag, task
-from airflow.models import Variable
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 from pendulum import datetime as pendulum_datetime
 from datetime import timedelta
-from business_logic.bbss_elt_pipeline import (
-    get_forecast, generate_s3_partitioned_key,
-    dump_json_to_s3, transform_forecast, send_parquet_to_s3
+from business_logic.bbss_weather_forecast.tasks import (
+    fetch, dump_raw_json, transform, send_transformed_parquet
 )
-from business_logic.slack_utils import slack_failure_alert, slack_success_alert
+from plugins.slack_utils import slack_failure_alert, slack_success_alert
 
-@dag(
+default_args={
+        "owner":"Federated Engineers",
+        "retries": 2,
+        "retry_delay": timedelta(seconds=100),
+        "on_failure_callback": slack_failure_alert,
+        "on_success_callback": slack_success_alert
+    }
+
+with DAG(
     dag_id="bbss_weather_forecast_ingestion_pipeline",
     schedule="0 23 * * *",
     start_date=pendulum_datetime(2026, 4, 23),
     catchup=False,
-    default_args={
-        "retries": 2,
-        "retry_delay": timedelta(seconds=100),
-	"on_failure_callback": slack_failure_alert,
-	"on_success_callback": slack_success_alert
-    },
+    default_args=default_args,
     tags=["Federated Engineers","forge","weather", "calvia"],
-)
-def weather_forecast_pipeline():
+) as dag:
 
-    @task()
-    def fetch():
-        return get_forecast()
+    fetch_task = PythonOperator(task_id='_fetch', python_callable=fetch)
+    dump_raw_json_task = PythonOperator(task_id='_dump_raw', python_callable=dump_raw_json)
+    transform_task = PythonOperator(task_id='_transform', python_callable=transform)
+    send_transformed_parquet_task = PythonOperator(task_id='_send_parquet', python_callable=send_transformed_parquet)
 
-    @task()
-    def dump_raw(nextday_forecast, bucket_name):
-        date_str = nextday_forecast[0]['time'].split(' ')[0]
-        key = generate_s3_partitioned_key("raw", date_str)
-        dump_json_to_s3(nextday_forecast, bucket_name, key)
-
-    @task()
-    def transform(nextday_forecast):
-        return transform_forecast(nextday_forecast)
-
-    @task()
-    def send_parquet(transformed, nextday_forecast, bucket_name):
-        date_str = nextday_forecast[0]['time'].split(' ')[0]
-        key = generate_s3_partitioned_key("transformed", date_str)
-        send_parquet_to_s3(transformed, bucket_name, key)
-
-    bucket_name = Variable.get("BUCKET_NAME")
-    nextday_forecast = fetch()
-    dump_raw(nextday_forecast, bucket_name)
-    transformed = transform(nextday_forecast)
-    send_parquet(transformed, nextday_forecast, bucket_name)
-
-weather_forecast_pipeline()
-
+    fetch_task >> dump_raw_json_task >> transform_task >> send_transformed_parquet_task
