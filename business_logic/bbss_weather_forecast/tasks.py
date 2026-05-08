8e5dad7bd64eaff9b38dcde8208e5ed566cd66e0
chik0di <chikodiobu@gmail.com>
2026-05-08 12:43:08 +0100

    modify fetch date logic get logical date from ds key in the context dictionary convert it from string object to date object add an extra day to the date pass this date into the function that extracts forecasts

--- business_logic/bbss_weather_forecast/tasks.py
+++ business_logic/bbss_weather_forecast/tasks.py
@@ -2,8 +2,8 @@ from datetime import date, timedelta
 
 from airflow.sdk import Variable
 
-from business_logic.bbss_weather_forecast.ingestion_pipeline import (get_forecast,
-                                                           send_parquet_to_s3)
+from business_logic.bbss_weather_forecast.ingestion_pipeline import (
+    get_forecast, send_parquet_to_s3)
 from plugins.aws import hive_partitioned_bucket_setup
 
 bbss_s3_bucket = Variable.get("BUCKET_NAME")
