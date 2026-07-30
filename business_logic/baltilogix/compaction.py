70c6fb209f69512fe5b6fad7b654a416c7908727
desayoem <labaekaadesayo@gmail.com>
2026-07-30 07:01:26 +0100

    Fix key access issues

--- business_logic/baltilogix/compaction.py
+++ business_logic/baltilogix/compaction.py
@@ -33,6 +33,7 @@ def process_and_count_objects(keys: List) -> Tuple[pd.DataFrame, int]:
         log.info(f"Processing {key}")
         parsed_key = "/".join(key.replace("s3://", "").split("/")[1:])
 
+
         content = s3_hook.read_key(key=parsed_key, bucket_name=src_bucket)
         content = json.loads(content)
         df = pd.json_normalize(content)
@@ -65,7 +66,7 @@ def run_compaction() -> Dict:
     src_bucket, src_prefix, dest_bucket = (
         config.get("BALTILOGIX_SOURCE"),
         config.get("BALTILOGIX_PATH"),
-        config.get("BALTILOGIX_BUCKET")
+        config.get("BALTILOGIX_DEST")
     )
     files = list_objects(src_bucket, src_prefix)
     data, source_count = process_and_count_objects(files)
